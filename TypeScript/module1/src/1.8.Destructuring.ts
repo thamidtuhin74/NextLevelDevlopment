0758f34088346cf2be7fd3fa999748a354a28c4c
thamidtuhin74 <altahmiduzzamantuhin74@gmail.com>
2025-05-02 17:26:22 +0600

    Desturcturing in Array & Object

--- TypeScript/module1/src/1.8.Destructuring.ts
+++ TypeScript/module1/src/1.8.Destructuring.ts
@@ -1,3 +1,25 @@
 {
   //use to define a scope to the typescript
+  //   Desturcturing 👇👇👇
+
+  //   --> Object Desturcturing👇👇
+  const user = {
+    id: 1001,
+    name: {
+      firstName: "Tahmid",
+      middlename: "",
+      lastName: "Tuhin",
+    },
+    contact: "01754555682",
+    address: "Rangpur",
+  };
+  const {
+    contact,
+    name: { lastName: nickNme }, //Name Alias
+  } = user;
+
+  //   --> Array Desturcturing👇👇
+
+  const myFriends: string[] = ["a", "b", "M", "S", "d", "e", "f"];
+  const [, , bestFriend1, bestFreiend2, ...goodFriend] = myFriends;
 }
