fe9f175533a9afff9a190de2a0fbce9ebe1882d6
thamidtuhin74 <altahmiduzzamantuhin74@gmail.com>
2025-05-09 02:46:12 +0600

    Interface & type vs interface done

--- TypeScript/module2/2.2.interface.ts
+++ TypeScript/module2/2.2.interface.ts
@@ -1,10 +1,11 @@
 {
   // Interface
-
+ //   Difference Between "Alias Type" & "Interface"👇👇👇
   type UserType = {
     name: string;
     age: number;
   };
+  // type RollNumber = number;
   const user1: UserType = {
     name: "Tahmid Tuhin",
     age: 27,
@@ -19,9 +20,85 @@
     name: "Tahmid Tuhin",
     age: 27,
   };
+  
+  // 1️⃣ for the primitive data type (number, string etc) will use "Alias Type"
+  // 1️⃣ for the object we will use "Interface"
+
+  // -----------------
+
+  type UserTypeWithRole  =  UserType & {role : string}; // Type Extend
+
+  const user1Extended: UserTypeWithRole = {
+    name: "Tahmid Tuhin",
+    age: 27,
+    role: "admin"
+  }
+
+  interface UserInterfaceExtended extends UserInterface{
+    role : string;
+  }
+  const user2extended: UserInterfaceExtended = {
+    name: "Tuhin",
+    age: 27,
+    role: "CEO"
+  }
+
+  interface UserInterfaceExtendedWithType extends UserType{ //Interface with Type
+     phone : number;
+  }
+  // 2️⃣ For extend Type Alias Use Intersection(&)
+  // 2️⃣ For extend Interface Use extend. | Interface can extend also with Type
+
+  // -----------------
+
+  // We know; In JS Objcet is an object | array & finction also -> Type of Object;
+  // 👉 3️⃣ for that we'll can use Interface for array & object
+
+  // Array Using Type👇👇👇
+  type Roll = number[];
+  const role: Roll = [2,8,5];
+  
+  // Array Using Interface👇👇👇
+
+  interface RollWithInterface{
+    [index: number]: number;
+    // [arrayIndex: typeOfIndex] : typeOfIndexValue;
+  }
+  const roleWithinterface = [2,8,29];
+
+
+  // Function Using Type👇👇👇
+  type Add = (num1: number, num2: number) => number;
+  const add:Add = (num1, num2) => num1+num2;
+
+  // Finctuin Using Interface👇👇👇
+
+  interface AddUnsingInterface{
+  (num1:number, num2: number):number;
+  }
+  const addUsingInterface:AddUnsingInterface = (num1, num2) => num1+num2;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   //   Difference Between "Alias Type" & "Interface"👇👇👇
 
   // 1️⃣ for the primitive data type (number, string etc) will use "Alias Type"
-  // 2️⃣ for the object we will use "Interface"
+  // 1️⃣ for the object we will use "Interface"
+
+  // -----------------
+  // 2️⃣ For extend Type Alias Use Intersection(&)
+  // 2️⃣ For extend Interface Use extend. | Interface can extend user also
 }
