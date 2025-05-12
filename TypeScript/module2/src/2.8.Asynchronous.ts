b2b8df6eac230240a20c42e1f1d78bf3ed26f00e
thamidtuhin74 <altahmiduzzamantuhin74@gmail.com>
2025-05-12 20:27:16 +0600

    updated Asynchronous

---
+++ TypeScript/module2/src/2.8.Asynchronous.ts
@@ -0,0 +1,16 @@
+{
+  // Promise👇👇👇
+  type Todo = {
+    userId: number;
+    id: number;
+    title: string;
+    completed: boolean;
+  };
+  const getTodo = async (): Promise<Todo> => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+    const data = await res.json();
+    console.log(data);
+    return data;
+  };
+  getTodo();
+}
