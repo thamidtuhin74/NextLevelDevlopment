b122ea727490968e76ad9bad93d63af0364f609c
thamidtuhin74 <altahmiduzzamantuhin74@gmail.com>
2025-05-15 22:07:12 +0600

    Type Guard

---
+++ TypeScript/module3/3.3.TypeGuard.ts
@@ -0,0 +1,41 @@
+{
+  // Type Guard
+  // We know: 👉👉typeof --> work on the RUN time;
+
+  // 1️⃣ typeof as Type Guard👇👇👇
+  type AlphaNeumeric = string | number;
+  const add = (param1: AlphaNeumeric, param2: AlphaNeumeric): AlphaNeumeric => {
+    if (typeof param1 === "number" && typeof param2 === "number")
+      return param1 + param2;
+    else {
+      return param1.toString() + param2.toString();
+    }
+  };
+  console.log(add(2, "5"));
+
+  // 2️⃣ In Guard👇👇👇
+
+  type NormalUser = {
+    name: string;
+  };
+  type AdminUser = {
+    name: string;
+    role: string;
+  };
+  const getUser = (user: NormalUser | AdminUser) => {
+    // console.log(user.)
+    if ("role" in user) {
+      console.log(`My name is ${user.name} & role is "${user.role}"`);
+    } else {
+      console.log(`My name is ${user.name} & role is not available`);
+    }
+  };
+  const user1: NormalUser = {
+    name: "Tuhin",
+  };
+  const user2: AdminUser = {
+    name: "CEO Tuhin",
+    role: "Author",
+  };
+  getUser(user2);
+}
