015d0d3ca45de49ec0b9f2bc577d9c158c1f6429
thamidtuhin74 <altahmiduzzamantuhin74@gmail.com>
2025-07-21 21:30:02 +0600

    Polymorphysom 2nd example

--- TypeScript/module3/3.8.polymorphysom.ts
+++ TypeScript/module3/3.8.polymorphysom.ts
@@ -31,4 +31,53 @@
   getSleepingHr(person3);
 
   //   Example 2 👇👇👇
+
+  class Shape {
+    getArea(): number {
+      return 0;
+    }
+  }
+
+  class Circle extends Shape {
+    redius: number;
+
+    constructor(redious: number) {
+      super();
+      this.redius = redious;
+    }
+
+    getArea(): number {
+      return Math.PI * this.redius * this.redius;
+    }
+  }
+
+  class rectangle extends Shape {
+    height: number;
+    width: number;
+    constructor(height: number, width: number) {
+      super();
+      this.height = height;
+      this.width = width;
+    }
+
+    getArea(): number {
+      return 0.5 * this.height * this.width;
+    }
+  }
+
+  const circle1 = new Circle(2);
+  // console.log(circle1.getArea());
+
+  const rectangle1 = new rectangle(5, 4);
+  // console.log(rectangle1.getArea());
+
+  const shape1 = new Shape();
+
+  const getShapeArea = (params: Shape) => {
+    console.log(params.getArea());
+  };
+
+  getShapeArea(circle1);
+  getShapeArea(rectangle1);
+  getShapeArea(shape1);
 }
